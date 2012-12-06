4f4171610e963ad85e6c5a51e6f50120d9498045
LIGHT <lightory@gmail.com>
2012-12-06 12:07:44 +0800

    add some methods to UINavigationController+HHKit

--- Controller/UINavigationController+HHKit.h
+++ Controller/UINavigationController+HHKit.h
@@ -3,6 +3,12 @@
 #import "HHBlocks.h"
 
 @interface UINavigationController (HHKit)
+- (void)customBackgroundWithImage:(UIImage *)image withShadowImage:(UIImage *)shadowImage;
+
+- (void)backBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage;
+- (void)leftBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage target:(id)target selector:(SEL)selector;
+- (void)rightBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage target:(id)target selector:(SEL)selector;
+
 - (void)pushViewControllerWithBlock:(HHViewControllerBlock)viewController animated:(BOOL)animated;
 - (void)popWithAnimated;
 
--- Controller/UINavigationController+HHKit.m
+++ Controller/UINavigationController+HHKit.m
@@ -3,6 +3,54 @@
 
 @implementation UINavigationController (HHKit)
 
+- (void)customBackgroundWithImage:(UIImage *)image withShadowImage:(UIImage *)shadowImage
+{
+    self.navigationBar.frame = CGRectMake(0, self.navigationBar.frame.origin.y, self.navigationBar.frame.size.width, 44);
+    
+    if ([self.navigationBar respondsToSelector:@selector(setBackgroundImage:forBarMetrics:)]) { // ios5
+        [self.navigationBar setBackgroundImage:image forBarMetrics:UIBarMetricsDefault];
+    }
+    
+    UIImageView *shadowView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, 5)];
+    shadowView.image = shadowImage;
+    [self.visibleViewController.view addSubview:shadowView];
+}
+
+- (void)backBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage
+{
+    [self leftBtnWithImage:image highlightImage:highlightImage target:self selector:@selector(popWithAnimated)];
+}
+
+- (void)leftBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage target:(id)target selector:(SEL)selector
+{
+    UIButton *leftBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    leftBtn.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    [leftBtn setImage:image forState:UIControlStateNormal];
+    
+    if (highlightImage) {
+        [leftBtn setImage:highlightImage forState:UIControlStateHighlighted];
+    }
+    
+    [leftBtn addTarget:target action:selector forControlEvents:UIControlEventTouchUpInside];
+    self.visibleViewController.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:leftBtn];
+}
+
+- (void)rightBtnWithImage:(UIImage *)image highlightImage:(UIImage *)highlightImage target:(id)target selector:(SEL)selector
+{
+    UIButton *rightBtn = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    rightBtn.frame = CGRectMake(0, 0, image.size.width, image.size.height);
+    [rightBtn setImage:image forState:UIControlStateNormal];
+    
+    if (highlightImage) {
+        [rightBtn setImage:highlightImage forState:UIControlStateHighlighted];
+    }
+    
+    [rightBtn addTarget:target action:selector forControlEvents:UIControlEventTouchUpInside];
+    self.visibleViewController.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:rightBtn];
+}
+
 - (void)pushViewControllerWithBlock:(HHViewControllerBlock)viewController animated:(BOOL)animated {
     [self pushViewController:viewController() animated:animated];
 }
